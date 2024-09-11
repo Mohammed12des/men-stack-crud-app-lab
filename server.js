1a056788bb2faebeeac8bcdbf5f7d9f91069f249
Mohammed12des <hamood12des@gmail.com>
2024-09-11 14:03:11 +0300

    done

--- server.js
+++ server.js
@@ -1,44 +1,33 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const methodOverride = require("method-override");
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-require('./config/database');
+require("./config/database");
 
-const Clothing = require('./modules/clothing');
+const Clothing = require("./modules/clothing");
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
-app.use(morgan ('dev'));
-
-
-
-  
-  
-
+app.use(morgan("dev"));
 
 app.get("/", async (req, res) => {
-    res.render("index.ejs")
-  });
+  res.render("index.ejs");
+});
 
-  app.get('/clothing/new', async(req,res)=>{
-   res.render("clothing/new.ejs")
-
-  })
-  app.post("/clothing", async (req, res) => {
-    console.log(req.body)
-    await Clothing.create(req.body);
-    res.redirect("/clothing/new");
+app.get("/clothing/new", async (req, res) => {
+  res.render("clothing/new.ejs");
+});
+app.post("/clothing", async (req, res) => {
+  await Clothing.create(req.body);
+  res.redirect("/clothing/new");
 });
 
 app.get("/clothing", async (req, res, next) => {
   const allclohting = await Clothing.find();
-  console.log(allclohting);
   res.render("clothing/index.ejs", { clothing: allclohting });
 });
-
-
 
 app.get("/clothing/:id", async (req, res, next) => {
   const foundClothing = await Clothing.findById(req.params.id);
@@ -52,19 +41,14 @@ app.delete("/clothing/:id", async (req, res, next) => {
 
 app.get("/clothing/:id/edit", async (req, res) => {
   const foundClothing = await Clothing.findById(req.params.id);
-  console.log(foundClothing);
   res.render("clothing/edit.ejs", {
-      clothing: foundClothing,
-    });
+    clothing: foundClothing,
+  });
 });
 
-
 app.put("/clothing/:id", async (req, res) => {
-    
-  
   await Clothing.findByIdAndUpdate(req.params.id, req.body);
 
-  // Redirect to the fruit's show page to see the updates
   res.redirect(`/clothing/${req.params.id}`);
 });
 
